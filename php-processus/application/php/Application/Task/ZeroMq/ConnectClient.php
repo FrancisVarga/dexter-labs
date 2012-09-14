02e1efbb352ee7ccfe893343b3acdad72eb3440d
Francis Varga <github@varga-net.com>
2012-09-14 23:57:18 +0200

    testing multiple non blocking request

--- php-processus/application/php/Application/Task/ZeroMq/ConnectClient.php
+++ php-processus/application/php/Application/Task/ZeroMq/ConnectClient.php
@@ -13,17 +13,35 @@ class ConnectClient extends \Processus\Abstracts\AbstractTask
     public function run()
     {
         /* Create new queue object */
-        $queue = new \ZMQSocket(new \ZMQContext(), \ZMQ::SOCKET_REQ, "MySock1");
+        $queue = new \ZMQSocket(new \ZMQContext(), \ZMQ::SOCKET_PUSH, "MySock1");
         $queue->connect("tcp://127.0.0.1:5555");
 
         $mqData = array(
             "id"        => 1,
-            "params"    => array("foobar"),
+            "params"    => array(array(
+                "message"  => "This is bullshit to.",
+                "created"  => time(),
+                "someData" => mt_rand(0, 49344409875093475),
+            )),
             "method"    => "ZeroMq.TestZeroMq.testEcho",
+            "extended"  => array(),
         );
 
         /* Assign socket 1 to the queue, send and receive */
-        $queue->send(json_encode($mqData));
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
+        $queue->send(json_encode($mqData), \ZMQ::MODE_NOBLOCK);
     }
 
     /**
