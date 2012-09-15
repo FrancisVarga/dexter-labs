719596893ffddbacc4d174dd1c319e83e638c86f
Francis Varga <github@varga-net.com>
2012-09-15 16:54:49 +0200

    class for fetching geo data

---
+++ php-processus/application/php/Application/Task/GoogleMaps/GettingGeoData.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hippsterkiller
+ * Date: 9/15/12
+ * Time: 3:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Application\Task\GoogleMaps;
+class GettingGeoData extends \Processus\Abstracts\AbstractTask
+{
+    public function run()
+    {
+        
+    }
+
+    /**
+     * @return string
+     */
+    protected function _getLogTable()
+    {
+        // TODO: Implement _getLogTable() method.
+    }
+
+    /**
+     * @param $rawObject
+     * @return array
+     */
+    protected function _getSqlLogParams($rawObject)
+    {
+        // TODO: Implement _getSqlLogParams() method.
+    }
+}
