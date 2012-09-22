b4057ae5ca22bbbd05ed1a308368b413c36e7131
Francis Varga <github@varga-net.com>
2012-09-22 11:27:38 +0200

    testing logging via zeromq and passing data

---
+++ php-processus/application/php/Application/Task/ZeroMq/CouchbaseLogging.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hippsterkiller
+ * Date: 9/21/12
+ * Time: 8:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CouchbaseLogging
+{
+
+}
