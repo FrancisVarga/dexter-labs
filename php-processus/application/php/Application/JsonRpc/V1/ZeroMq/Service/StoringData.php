209a9769e3afa8439dd1d1f269eb001768f0befe
Francis Varga <github@varga-net.com>
2012-09-23 04:40:24 +0200

    api dor storing data

---
+++ php-processus/application/php/Application/JsonRpc/V1/ZeroMq/Service/StoringData.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hippsterkiller
+ * Date: 9/23/12
+ * Time: 4:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class StoringData
+{
+
+}
