d768f72c75a840d3e8b61d7ed7469bca29de4504
Francis Varga <github@varga-net.com>
2012-09-23 17:30:10 +0200

    adding some nodejs stuff

---
+++ nodejs/helloWorld/routes/contact.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: hippsterkiller
+ * Date: 9/18/12
+ * Time: 3:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
--- php-processus/application/php/Application/Task/Couchbase/SyncCB.php
+++ php-processus/application/php/Application/Task/Couchbase/SyncCB.php
@@ -14,12 +14,10 @@ class SyncCB extends \Processus\Abstracts\AbstractTask
     {
         echo "Sync Starting" . PHP_EOL;
 
-        $targetHost     = "couchbase-server";
-        $targetPort     = "11211";
-        $remoteApiUrl   = "http://ec2-54-247-28-110.eu-west-1.compute.amazonaws.com:8092";
-        $remoteBucket   = "casino";
-        $remoteViewName = "all";
-        $remoteDesign   = "portal";
+        $targetHost   = "127.0.0.1";
+        $targetPort   = "11211";
+        $remoteApiUrl = "http://ec2-79-125-38-103.eu-west-1.compute.amazonaws.com:8092";
+        $remoteBucket = "default";
 
 //        $cbAdapater = new \Application\Utils\CBAdapter();
 //        $allData    = $cbAdapater->setApiUrl($remoteApiUrl)
@@ -31,13 +29,15 @@ class SyncCB extends \Processus\Abstracts\AbstractTask
 //            )
 //        );
 
-        $allData    = json_decode(file_get_contents("http://ec2-54-247-28-110.eu-west-1.compute.amazonaws.com:8092/casino/_all_docs?include_docs=true"), TRUE);
+        $gatewayUrl = $remoteApiUrl . "/" . $remoteBucket . "/_all_docs?include_docs=true";
+        var_dump($gatewayUrl);
+        $allData    = json_decode(file_get_contents($gatewayUrl), TRUE);
         $totalItems = count($allData['rows']);
 
         echo "Total Sync items: $totalItems" . PHP_EOL;
 
         try {
-            $insertAdapter = new \Application\Core\Lib\Db\MemcachedJson($targetHost, $targetPort);
+            $insertAdapter = new \Processus\Lib\Db\MemcachedJson($targetHost, $targetPort);
             $result        = $insertAdapter->insert("foo", array("name" => "bar_" . mt_rand(0, 1394898304))); // Testing if couchbase responding or not! Expiredtime is 1 sec.
         } catch (\Exception $error) {
             var_dump($error);
