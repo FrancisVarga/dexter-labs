74f39ec6c4157e29f819f43da7c6a6a6cd9b4791
Francis Varga <github@varga-net.com>
2012-09-23 17:26:33 +0200

    adding new Storing API

--- php-processus/application/php/Application/JsonRpc/V1/ZeroMq/Service/StoringData.php
+++ php-processus/application/php/Application/JsonRpc/V1/ZeroMq/Service/StoringData.php
@@ -6,7 +6,33 @@
  * Time: 4:37 AM
  * To change this template use File | Settings | File Templates.
  */
-class StoringData
+namespace Application\JsonRpc\V1\ZeroMq\Service;
+class StoringData extends \Processus\Abstracts\JsonRpc\AbstractJsonRpcService
 {
+    public function storeData($params)
+    {
+        $data       = $params['data'];
+        $serverData = $params['serverData'];
 
+        if (empty($serverData['host'])) {
+            $serverData['host'] = "localhost:8091";
+        }
+
+        if (empty($serverData['user'])) {
+            $serverData['user'] = "Administrator";
+        }
+
+        if (empty($serverData['password'])) {
+            $serverData['password'] = "Administrator";
+        }
+
+        if (empty($serverData['bucket'])) {
+            $serverData['bucket'] = "default";
+        }
+
+        $couchCli = new \Processus\Lib\Db\CouchbaseClient($serverData['host'], $serverData['user'], $serverData['password'], $serverData['bucket']);
+        $couchCli->insert($data['id'], $data['value'], $data['expiredTime']);
+
+        return;
+    }
 }
